08a59ae03366eac31c30a07e27cbbe04afa22f53
cesar <vcesar2021@gmail.com>
2023-12-15 15:01:17 -0500

    packages updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot(),
 
-    MongooseModule.forRoot(process.env.MONGO_URI, {
+    MongooseModule.forRoot( process.env.MONGO_URI, {
       dbName: process.env.MONGO_DB_NAME,
     }),
 
