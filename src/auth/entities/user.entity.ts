ca390e72ca4bcb1f6fb8a482c135e332ea749255
cesar <vcesar2021@gmail.com>
2023-12-15 13:08:43 -0500

    first commit

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+
+@Schema()
+export class User {
+
+    _id?:string;
+
+    // Mongo coloca el id automáticamente (identity)
+    @Prop({ unique: true, required:true })
+    email: string;
+
+    @Prop({ required:true })
+    name:string;
+
+    @Prop({ minlength:6, required:true })
+    password?: string;
+
+    @Prop({ default:true })
+    isActive: boolean;
+
+    @Prop({ type:[String], default:['user'] })
+    roles: string[];
+
+}
+
+export const UserSchema = SchemaFactory.createForClass( User );
